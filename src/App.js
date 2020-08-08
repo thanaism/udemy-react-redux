9989f9e209cd73d8795b5a7d9c3d2af5ac360c23
thanai-mac <examle@examle.com>
2020-08-09 07:19:51 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -13,39 +13,32 @@ import propTypes from 'prop-types';
 //   }
 // }
 
-const App = () => {
-  const profiles = [
-    {name: 'Taro', age:10},
-    {name: 'Hanako', age:5},
-    {name: 80}
-  ];
-  return (
-    <React.Fragment>
-      {/* <User 
-        name={"Taro"} 
-        age={10}
-      />
-      <User 
-        name={"Hanako"} 
-        age={5}
-      /> */}
-      {
-        profiles.map((profile, index)=>{
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </React.Fragment>
-  );
+const App = () => (<Counter></Counter>)
+
+class Counter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {count: 0};
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render () {
+    // console.log('render');
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
-const User = (props) => {
-  return <div>I am {props.name} and {props.age} years old!</div>;
-}
-
-// User.defaultProps = {
-//   age: 1
-// }
-User.propTypes = {
-  name: propTypes.string
-}
 export default App;
