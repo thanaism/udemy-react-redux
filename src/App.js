56526493d9c1fd1182d723404542470011f41a4d
thanai-mac <examle@examle.com>
2020-08-08 22:32:03 +0900

    learn syntax jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react';
 //import './App.css';
 
 function App() {
-  return <div><h1>Hello, World!</h1></div>;
+  const dom = <h1>Hi!</h1>;
+  return (
+    <React.Fragment>
+      <p>pragraph1</p>
+      <p>pragraph2</p>
+    </React.Fragment>
+  );
 }
+
+// class App extends React.Component{
+//   render(){
+//     return (
+//       React.createElement(
+//         'div',
+//         null,
+//         'Hello, React!'
+//       )
+//     );
+//   }
+// }
 
 export default App;
